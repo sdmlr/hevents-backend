116446428243cea2ba425996b4a0e29a83e7d8b2
Stefano Di Michele <sdmlr_@hotmail.com>
2025-04-07 23:38:29 +0100

    functional code setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import eventsRouter from './routes/events';
 
 dotenv.config();
 
@@ -8,9 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hevents backend is running 🚀');
-});
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hevents backend is running 🚀');
+  });
+
+app.use('/events', eventsRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
